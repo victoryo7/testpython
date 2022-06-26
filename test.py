4dc847e88e016c7a73e5f302187afa442335ec04
LogitSocial <108209621+LogitSocial@users.noreply.github.com>
2022-06-26 13:13:28 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,17 @@
-import os
+from authlib.jose import jwt
+import time
+import json
+import urllib
+from urllib.request import urlopen
+import requests
+import urllib
+import http.client
+import sys
+import pandas as pd
+import numpy as np
+import math
+from itertools import cycle
+
 inputvariable = os.environ['script_path']
 print(inputvariable)
 print('Hello World!')
